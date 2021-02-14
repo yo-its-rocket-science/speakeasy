668dceefbe5a293c2ee4677f2d6ea10435bb36c2
Daniel <daniel.artuso1@gmail.com>
2021-02-13 23:37:18 -0700

    fix LoginScreen.tsx

--- screens/Calendar/navigator.tsx
+++ screens/Calendar/navigator.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Calendar } from "../Calendar";
+import { Calendar } from "./index";
 import { CalendarScreenParamList } from "../../types";
 
 const CalendarStack = createStackNavigator<CalendarScreenParamList>();
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -5,6 +5,7 @@ import { Text, View } from "../components/Themed";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../types";
 import { theme } from "../theme";
+import { firebase } from "@react-native-firebase/auth";
 
 type ProfileScreenNavigationProp = StackNavigationProp<RootStackParamList, "Root">;
 
@@ -15,31 +16,45 @@ type Props = {
 export default function LoginScreen({ navigation }: Props) {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [errorMessage, setErrorMsg] = React.useState("");
+
+  function AttempSignIn() {
+    if (email.length === 0 || password.length === 0) {
+      setErrorMsg("Enter your email and password to login.");
+    } else {
+      firebase.auth().signInWithEmailAndPassword(email, password).then((userCrendential) => {
+        navigation.navigate("Root");
+      }).catch((error) => {
+        setErrorMsg(error.message);
+      });
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>SpeakEasy</Text>
+      <Text>{errorMessage}</Text>
       <TextInput label="Email" style={styles.field} value={email} onChangeText={text => setEmail(text)}
                  accessibilityComponentType="" accessibilityTraits="" textContentType="emailAddress"
                  autoCompleteType="email" keyboardType="email-address" />
       <TextInput label="Password" style={styles.field} value={password} onChangeText={text => setPassword(text)}
                  accessibilityComponentType="" accessibilityTraits="" secureTextEntry={true} textContentType="password"
                  autoCompleteType="password" />
-      <Button mode="contained" style={styles.button} color="black" onPress={() => navigation.navigate("Root")}
-              accessibilityComponentType="" accessibilityTraits="">Sign up</Button>
+      <Button mode="contained" style={styles.button} color="black" onPress={AttempSignIn} accessibilityComponentType=""
+              accessibilityTraits="">Log In</Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    width: '40%',
-    marginTop: '5%',
-    marginLeft: '20%'
+    width: "40%",
+    marginTop: "5%",
+    marginLeft: "20%",
   },
   field: {
-    width: '80%',
-    marginTop: '5%',
+    width: "80%",
+    marginTop: "5%",
   },
   container: {
     backgroundColor: theme.colors.background,
