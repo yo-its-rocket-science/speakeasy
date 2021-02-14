b56c3f75fbe72234c48c8aa1294e671f6fd03102
Daniel <daniel.artuso1@gmail.com>
2021-02-13 20:24:32 -0700

    fixed state error

--- screens/LandingScreen.tsx
+++ screens/LandingScreen.tsx
@@ -2,15 +2,26 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
 import { Text, View } from '../components/Themed';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from "../types";
+type ProfileScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Login'
+  >;
 
-export default function LandingScreen({navigation}) {
+
+type Props = {
+  navigation: ProfileScreenNavigationProp;
+};
+
+export default function LandingScreen({navigation}: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>SpeakEasy</Text>
       <Button mode="contained" style={styles.button} color="black" onPress={() => navigation.navigate('Login')}
               accessibilityComponentType=""
               accessibilityTraits="">Log in</Button>
-      <Button mode="contained" style={styles.button} color="black" onPress={() => navigation.navigate('SignUp')}
+      <Button mode="contained" style={styles.button} color="black" onPress={() => navigation.navigate('Login')}
               accessibilityComponentType=""
               accessibilityTraits="">Sign up</Button>
     </View>
