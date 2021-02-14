cf255f6657cb4b11faf05fb897caf0fade02fe76
Alex Zecevic <alexzecevic15@gmail.com>
2021-02-14 01:37:04 -0700

    adds profile setup skeletion

---
+++ screens/ProfileSetup.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import { Text, View } from "../components/Themed";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../types";
+import { theme } from "../theme";
+
+export default function ProfileSetup () {
+    
+}
